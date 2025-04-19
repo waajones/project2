b08ccbf3aa4e234fe997c12af8357f133c6d6dd1
williaj <williaj@siue.edu>
2025-04-18 20:22:11 -0500

    1

--- mipssim.cpp
+++ mipssim.cpp
@@ -11,15 +11,11 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-
   struct memItem {
-    int I, opcode, rs, rt, rd, shamt, func, imm, jtarget, boffset; 
-    int src1, src2, dest;
-    unsigned int UI;
-    string binstr; 
-    string binstrNoSpace;
-    string instr, outline;
-    memItem():dest(-2) { }
+    int I = 0, opcode = 0, rs = 0, rt = 0, rd = 0, shamt = 0, func = 0, imm = 0, jtarget = 0, boffset = 0;
+    int src1 = 0, src2 = 0, dest = -2;
+    unsigned int UI = 0;
+    string binstr = "", binstrNoSpace = "", instr = "", outline = "";
   };
 
   unordered_map<int, memItem> MEM;
@@ -28,25 +24,23 @@ int main(int argc, char *argv[])
   int I;
   char * iPtr;
   iPtr = (char*)(void*) &I;
-  int count = 0;
   int FD = open(argv[2], O_RDONLY);
-  string outdisstr = string(argv[4] ) + "_dis.txt";
-  ofstream outdis( outdisstr );
+  string outdisstr = string(argv[4]) + "_dis.txt";
+  ofstream outdis(outdisstr);
   int address = 96;
   int amt = 4;
   bool seenBreak = false;
-  while( amt != 0 )
-  {
+
+  while (amt != 0) {
     amt = read(FD, buffer, 4);
-    if( amt == 4)
-    {
+    if (amt == 4) {
       iPtr[0] = buffer[3];
       iPtr[1] = buffer[2];
       iPtr[2] = buffer[1];
       iPtr[3] = buffer[0];
       memItem M;
       M.I= I;
-      M.UI = static_cast<unsigned int>( I );
+      M.UI = static_cast<unsigned int>(I);
       M.opcode = M.UI >> 26;
       M.rs = M.UI << 6 >> 27;
       M.rt = M.UI << 11 >> 27;
@@ -61,283 +55,215 @@ int main(int argc, char *argv[])
       M.binstr.insert(26," ").insert(21," ").insert(16," ").insert(11," ").insert(6," ");
       string instr;
       string outline;
-      //binstrSpace = binstrSpace;
-      if(! seenBreak ) {  
+      if (!seenBreak) {
         outline = M.binstr + "\t";
-        // cout << outline;
-        if( M.I == 0 ) {  //NOP
+        if (M.I == 0) {
           instr += "\tNOP";
           outline += to_string(address) + instr;
-          M.src1 = 0;
-          M.src2 = 0;
-          M.dest = 0;
-        } else if( M.opcode == 8 ){  // ADDI
+          M.src1 = 0; M.src2 = 0; M.dest = 0;
+        } else if (M.opcode == 8) {
           instr += "\tADDI\tR" + to_string(M.rt) + ", R" + to_string(M.rs) + ", #" + to_string(M.imm);
-          outline += to_string(address) + instr; 
-          M.src1 = M.rs;
-          M.src2 = M.rs;
-          M.dest = M.rt;
-        } else if( M.opcode == 43 ){ //SW
+          outline += to_string(address) + instr;
+          M.src1 = M.rs; M.src2 = M.rs; M.dest = M.rt;
+        } else if (M.opcode == 43) {
           instr += "\tSW\tR" + to_string(M.rt) + ", " + to_string(M.imm) + "(R" + to_string(M.rs) + ")";
           outline += to_string(address) + instr;
-          M.src1 = M.rs;
-          M.src2 = M.rt;
-          M.dest = -1;
-        } else if( M.opcode == 35 ){ //LW
+          M.src1 = M.rs; M.src2 = M.rt; M.dest = -1;
+        } else if (M.opcode == 35) {
           instr += "\tLW\tR" + to_string(M.rt) + ", " + to_string(M.imm) + "(R" + to_string(M.rs) + ")";
           outline += to_string(address) + instr;
-          M.src1 = M.rs;
-          M.src2 = M.rs;
-          M.dest = M.rt;
-        } else if( M.opcode == 1 ){  // BLTZ
+          M.src1 = M.rs; M.src2 = M.rs; M.dest = M.rt;
+        } else if (M.opcode == 1) {
           instr += "\tBLTZ\tR" + to_string(M.rs) + ", #" + to_string(M.boffset);
           outline += to_string(address) + instr;
-          M.src1 = M.rs;
-          M.src2 = M.rs;
-          M.dest = -1;
-        } else if( M.opcode == 0 && M.func == 0){  // SLL
+          M.src1 = M.rs; M.src2 = M.rs; M.dest = -1;
+        } else if (M.opcode == 0 && M.func == 0) {
           instr += "\tSLL\tR" + to_string(M.rd) + ", R" + to_string(M.rt) + ", #" + to_string(M.shamt);
           outline += to_string(address) + instr;
-          M.src1 = M.rt;
-          M.src2 = M.rt;
-          M.dest = M.rd;
-        } else if( M.opcode == 0 && M.func == 2){  // SRL
+          M.src1 = M.rt; M.src2 = M.rt; M.dest = M.rd;
+        } else if (M.opcode == 0 && M.func == 2) {
           instr += "\tSRL\tR" + to_string(M.rd) + ", R" + to_string(M.rt) + ", #" + to_string(M.shamt);
           outline += to_string(address) + instr;
-          M.src1 = M.rt;
-          M.src2 = M.rt;
-          M.dest = M.rd;
-        } else if( M.opcode == 0 && M.func == 34){  // SUB
+          M.src1 = M.rt; M.src2 = M.rt; M.dest = M.rd;
+        } else if (M.opcode == 0 && M.func == 34) {
           instr += "\tSUB\tR" + to_string(M.rd) + ", R" + to_string(M.rs) + ", R" + to_string(M.rt);
           outline += to_string(address) + instr;
-          M.src1 = M.rs;
-          M.src2 = M.rt;
-          M.dest = M.rd;
-        } else if( M.opcode == 28 && M.func == 2){  // MUL
+          M.src1 = M.rs; M.src2 = M.rt; M.dest = M.rd;
+        } else if (M.opcode == 28 && M.func == 2) {
           instr += "\tMUL\tR" + to_string(M.rd) + ", R" + to_string(M.rs) + ", R" + to_string(M.rt);
           outline += to_string(address) + instr;
-          M.src1 = M.rs;
-          M.src2 = M.rt;
-          M.dest = M.rd;
-        } else if( M.opcode == 0 && M.func == 10){  // MOVZ
+          M.src1 = M.rs; M.src2 = M.rt; M.dest = M.rd;
+        } else if (M.opcode == 0 && M.func == 10) {
           instr += "\tMOVZ\tR" + to_string(M.rd) + ", R" + to_string(M.rs) + ", R" + to_string(M.rt);
           outline += to_string(address) + instr;
-          M.src1 = M.rs;
-          M.src2 = M.rt;
-          M.dest = M.rd;
-        }  else if( M.opcode == 2 ){  // J
+          M.src1 = M.rs; M.src2 = M.rt; M.dest = M.rd;
+        } else if (M.opcode == 2) {
           instr += "\tJ\t#" + to_string(M.jtarget);
           outline += to_string(address) + instr;
-          M.src1 = -1;
-          M.src2 = -1;
-          M.dest = 0;
-        }  else if( M.opcode == 0 && M.func == 32){  // ADD
+          M.src1 = -1; M.src2 = -1; M.dest = 0;
+        } else if (M.opcode == 0 && M.func == 32) {
           instr += "\tADD\tR" + to_string(M.rd) + ", R" + to_string(M.rs) + ", R" + to_string(M.rt);
           outline += to_string(address) + instr;
-          M.src1 = M.rs;
-          M.src2 = M.rt;
-          M.dest = M.rs;
-        }  else if( M.opcode == 0 && M.func == 13 ){  // BREAK
+          M.src1 = M.rs; M.src2 = M.rt; M.dest = M.rd;
+        } else if (M.opcode == 0 && M.func == 13) {
           instr += "\tBREAK\t";
           outline += to_string(address) + instr;
           seenBreak = true;
-          firstMem = address+4;
-          M.src1 = -1;
-          M.src2 = -1;
-          M.dest = 0;
-        } else if( M.opcode == 0 && M.func == 8){  // JR
-          instr += "\tJR\tR" + to_string(M.rs); 
+          firstMem = address + 4;
+          M.src1 = -1; M.src2 = -1; M.dest = 0;
+        } else if (M.opcode == 0 && M.func == 8) {
+          instr += "\tJR\tR" + to_string(M.rs);
           outline += to_string(address) + instr;
-          M.src1 = M.rs;
-          M.src2 = M.rs;
-          M.dest = 0;
-        } 
-        else {
-          cerr << "Unkown opcode: " << M.opcode << endl;
-          exit( 0 );
+          M.src1 = M.rs; M.src2 = M.rs; M.dest = 0;
+        } else {
+          cerr << "Unknown opcode: " << M.opcode << endl;
+          exit(0);
         }
         M.outline = outline;
-      } else // end of processing opcodes,  for th rest process memory
-      {
-        instr = M.binstrNoSpace + "\t";
-        instr += to_string(address) + " " +to_string(M.I); 
+      } else {
+        instr = M.binstrNoSpace + "\t" + to_string(address) + " " + to_string(M.I);
         M.outline = instr;
       }
       M.instr = instr;
       MEM[address] = M;
       outdis << M.outline << endl;
       lastMem = address;
-      address +=4;
+      address += 4;
     }
   }
   outdis.close();
 
-  // start sim
-  //
-  //
-  string outsimstr = string(argv[4] ) + "_sim.txt";
-  ofstream outsim( outsimstr );
-  
+  string outsimstr = string(argv[4]) + "_sim.txt";
+  ofstream outsim(outsimstr);
+
   struct processor {
+    unordered_map<int, memItem> MEM;
     int PC=96, R[32]={0}, cycle=1, fetch, firstMem, lastMem;
     memItem preIssue[4], preALU[2], preMem[2], postALU, postMem;
-    unordered_map<int, memItem> MEM;
 
-    processor( unordered_map<int, memItem> MEM, int _firstMem, int _lastMem ):
-      MEM( MEM ), firstMem(_firstMem), lastMem(_lastMem)
-    { }
-    void IF(){
-      fetch=PC;
-      for( int i = 0; i < 2; i++ ){
-        if( preIssue[3].instr != "" ) break;
+    processor(unordered_map<int, memItem> MEM, int _firstMem, int _lastMem):
+      MEM(MEM), firstMem(_firstMem), lastMem(_lastMem) {}
 
+    void IF() {
+      fetch = PC;
+      for (int i = 0; i < 2; i++) {
+        if (preIssue[3].instr != "") break;
         memItem I = MEM[PC];
-        // check for branch or jump
-        //
-        int j =0;
-        for( ; j<4; j++ )
-          if( preIssue[j].instr == "" ) break;
+        int j = 0;
+        for (; j < 4; j++)
+          if (preIssue[j].instr == "") break;
         preIssue[j] = I;
-
         PC += 4;
       }
-        
     }
-    bool XBW( int r, int start){
-      for( int i = start; i >=0; i--)
-        if( preIssue[i].dest == r ) return true;
-      if( preMem[1].instr != "" && preMem[1].dest == r ) return true;
-      if( preMem[1].dest == r ) return true;
-      if( preALU[0].dest == r ) return true;
-      if( preALU[0].dest == r ) return true;
-      if( postALU.dest == r ) return true;
-      if( postMem.dest == r ) return true;
+
+    bool XBW(int r, int start) {
+      for (int i = start; i >= 0; i--)
+        if (preIssue[i].dest == r) return true;
+      if (preMem[1].instr != "" && preMem[1].dest == r) return true;
+      if (preALU[0].dest == r) return true;
+      if (postALU.dest == r) return true;
+      if (postMem.dest == r) return true;
       return false;
     }
-    void ISSUE(){
-      for( int i = 0; i< 4; i++ ){
-        cout << "preissue " << preIssue[i].instr << endl;
-        if( preIssue[i].instr == "" ) continue;
-        cout << "0" << endl;
-        if( XBW( preIssue[i].src1, i-1 ) ) continue;
-        cout << "1" << endl;
-        if( XBW( preIssue[i].src2, i-1 ) ) continue;
-        cout << "2" << endl;
-        if( XBW( preIssue[i].dest, i-1 ) ) continue;
-        cout << "3" << endl;
-        if( preIssue[i].opcode == 35 || preIssue[i].opcode == 43 ) { //35 = lw 43 = sw
-          if( preMem[0].instr == "" ) {
+
+    void ISSUE() {
+      for (int i = 0; i < 4; i++) {
+        if (preIssue[i].instr == "") continue;
+        if (XBW(preIssue[i].src1, i-1)) continue;
+        if (XBW(preIssue[i].src2, i-1)) continue;
+        if (XBW(preIssue[i].dest, i-1)) continue;
+        if (preIssue[i].opcode == 35 || preIssue[i].opcode == 43) {
+          if (preMem[0].instr == "") {
             preMem[0] = preIssue[i];
             preIssue[i] = memItem();
-          } else if( preMem[1].instr == "" ) {
+          } else if (preMem[1].instr == "") {
             preMem[1] = preIssue[i];
             preIssue[i] = memItem();
-          } else continue;
+          }
         }
-
       }
     }
-    void PRINT(){
 
+    void ALU() {
+      if (preALU[0].instr != "") {
+        memItem I = preALU[0];
+        if (I.opcode == 8) postALU = I, postALU.I = R[I.rs] + I.imm;
+        else if (I.opcode == 0 && I.func == 32) postALU = I, postALU.I = R[I.rs] + R[I.rt];
+        else if (I.opcode == 0 && I.func == 34) postALU = I, postALU.I = R[I.rs] - R[I.rt];
+        else if (I.opcode == 28 && I.func == 2) postALU = I, postALU.I = R[I.rs] * R[I.rt];
+        else if (I.opcode == 0 && I.func == 10) { postALU = I; if (R[I.rt] == 0) postALU.I = R[I.rs]; else postALU.dest = -1; }
+        else if (I.opcode == 0 && I.func == 0) postALU = I, postALU.I = R[I.rt] << I.shamt;
+        else if (I.opcode == 0 && I.func == 2) postALU = I, postALU.I = R[I.rt] >> I.shamt;
+        preALU[0] = memItem();
+      }
+    }
+
+    void MEM1() {
+      if (preMem[0].instr != "") {
+        memItem I = preMem[0];
+        if (I.opcode == 35) {
+          postMem = I;
+          int addr = R[I.rs] + I.imm;
+          postMem.I = MEM[addr].I;
+        } else if (I.opcode == 43) {
+          int addr = R[I.rs] + I.imm;
+          MEM[addr].I = R[I.rt];
+          postMem = memItem();
+        }
+        preMem[0] = preMem[1];
+        preMem[1] = memItem();
+      }
+    }
+
+    void WB() {
+      if (postALU.dest >= 0) R[postALU.dest] = postALU.I;
+      if (postMem.dest >= 0) R[postMem.dest] = postMem.I;
+      postALU = memItem();
+      postMem = memItem();
+    }
+
+    void PRINT() {
       string C("--------------------\ncycle:");
-      C+= to_string(cycle) + "\t" +to_string(fetch);
-      C+= "\n\nPre-Issue Buffer:";
-      for( int i = 0; i < 4; i++){
-        C += "\n\tEntry " + to_string(i) + ":\t" + preIssue[i].instr;
-      }
-      C+= "\nPre_ALU Queue:";
-       for( int i = 0; i < 2; i++){
-        C += "\n\tEntry " + to_string(i) + ":\t" + preALU[i].instr;
-      }
-      C+= "\nPost_ALU Queue:\n\tEntry 0:\t" + postALU.instr;
-
-       C+= "\nPre_MEM Queue:";
-       for( int i = 0; i < 2; i++){
-        C += "\n\tEntry " + to_string(i) + ":\t" + preMem[i].instr;
-      }
-      C+= "\nPost_MEM Queue:\n\tEntry 0:\t" + postMem.instr;
-
-      C +="\n\nRegisters:";
-      for( int i = 0; i < 32; i++){
-        if( i % 8 ==  0 ){
-          C+="\nr";
+      C += to_string(cycle) + "\t" + to_string(fetch);
+      C += "\n\nPre-Issue Buffer:";
+      for (int i = 0; i < 4; i++) C += "\n\tEntry " + to_string(i) + ":\t" + preIssue[i].instr;
+      C += "\nPre_ALU Queue:";
+      for (int i = 0; i < 2; i++) C += "\n\tEntry " + to_string(i) + ":\t" + preALU[i].instr;
+      C += "\nPost_ALU Queue:\n\tEntry 0:\t" + postALU.instr;
+      C += "\nPre_MEM Queue:";
+      for (int i = 0; i < 2; i++) C += "\n\tEntry " + to_string(i) + ":\t" + preMem[i].instr;
+      C += "\nPost_MEM Queue:\n\tEntry 0:\t" + postMem.instr;
+      C += "\n\nRegisters:";
+      for (int i = 0; i < 32; i++) {
+        if (i % 8 == 0) {
+          C += "\nr";
           string num = to_string(i);
-          if( i < 10) num.insert(0,"0");
+          if (i < 10) num.insert(0,"0");
           C += num + ":";
         }
-        C+= "\t" + to_string(R[i] );
+        C += "\t" + to_string(R[i]);
       }
-
       C += "\n\ndata:";
-      for( int i = firstMem; i <= lastMem; i+=4){
-        if( (i-firstMem) % 32 == 0 ){
-          C += "\n" + to_string(i) + ":";
-        }
-        C+= "\t" + to_string(MEM[i].I);
+      for (int i = firstMem; i <= lastMem; i += 4) {
+        if ((i - firstMem) % 32 == 0) C += "\n" + to_string(i) + ":";
+        C += "\t" + to_string(MEM[i].I);
       }
-      cout << C<<endl;
-
+      cout << C << endl;
     }
   };
-  processor P(MEM, firstMem, lastMem);
-  seenBreak = false;
-  while( true ){
-    
-    if( P.cycle >2 ) exit(0);
-
-    //P.WB();
-    //P.MEM();
-    //P.ALU();
-    P.ISSUE();
-    P.IF();
-    P.PRINT();
-    //if( seenBreak ) break;
-    P.cycle++;
-  }
-
-
+processor P(MEM, firstMem, lastMem);
+while (true) {
+  if (P.MEM[P.PC].opcode == 0 && P.MEM[P.PC].func == 13) break;
+  P.WB();
+  P.MEM1();
+  P.ALU();
+  P.ISSUE();
+  P.IF();
+  P.PRINT();
+  P.cycle++;
+ }
+P.PRINT();
+return 0;
 }
 
-
-    /*string outline;
-    fetch = PC;
-    M = MEM[PC];
-    
-      if( M.I == 0 ) {  //NOP
-      } else if( M.opcode == 8 ){  // ADDI
-        R[M.rt] = R[M.rs]+M.imm;
-      } else if( M.opcode == 43 ){ //SW
-        MEM[ R[M.rs] + M.imm].I = R[M.rt];
-      } else if( M.opcode == 35 ){ //LW
-        R[M.rt] = MEM[R[M.rs] + M.imm].I;
-      } else if( M.opcode == 1 ){  // BLTZ
-        if( R[M.rs] < 0)
-          PC += M.boffset;
-      } else if( M.opcode == 0 && M.func == 0){  // SLL
-        R[M.rd] = R[M.rt] << M.shamt;
-      } else if( M.opcode == 0 && M.func == 2){  // SRL
-        R[M.rd] = R[M.rt] >> M.shamt;
-      } else if( M.opcode == 0 && M.func == 34){  // SUB
-        R[M.rd] = R[M.rs] - R[M.rt];
-      } else if( M.opcode == 28 && M.func == 2){  // MUL
-        R[M.rd] = R[M.rs] * R[M.rt];
-        //outline = "\tMUL\tR" + to_string(rd) + ", R" + to_string(rs) + ", R" + to_string(rt);
-        //instr += to_string(address) + outline;
-      } else if( M.opcode == 0 && M.func == 10){  // MOVZ
-        if( R[M.rt] == 0 ) R[M.rd] = R[M.rs];
-        //outline = "\tMOVZ\tR" + to_string(rd) + ", R" + to_string(rs) + ", R" + to_string(rt);
-        //instr += to_string(address) + outline;
-      }  else if( M.opcode == 2 ){  // J
-        PC = M.jtarget-4;
-      }  else if( M.opcode == 0 && M.func == 32){  // ADD
-        R[M.rd] = R[M.rs] + R[M.rt];
-      }  else if( M.opcode == 0 && M.func == 13 ){  // BREAK
-        seenBreak = true;
-      } else if( M.opcode == 0 && M.func == 8){  // JR
-        PC = R[M.rs]-4;
-      } 
-      else {
-        cerr << "Unkown opcode: " << M.opcode << endl;
-        exit( 0 );
-      }
-*/
